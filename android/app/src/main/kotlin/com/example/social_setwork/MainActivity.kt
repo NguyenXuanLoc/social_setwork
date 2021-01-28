45537ae499121d14065cf8ee3f25a031850b10e6
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2021-01-28 17:21:24 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/social_setwork/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.social_setwork
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+
+
+class MainActivity : FlutterActivity(), MethodChannel.MethodCallHandler {
+    private val CHANNEL = "flutter.native/playerVideo"
+    private val openBroswer = "openBroswer"
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        MethodChannel(getBinaryMessenger(), CHANNEL).setMethodCallHandler(this)
+
+    }
+
+    fun getBinaryMessenger(): BinaryMessenger? {
+        return flutterEngine!!.dartExecutor.binaryMessenger
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+        when (call.method) {
+            openBroswer -> {
+                Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:social_setwork/screen/twitter_screen.dart';
+
+void main() {
+  runApp(TwitterScreen());
+}
